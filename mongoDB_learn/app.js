219ab6b2e307deefc33b191982cea33b1deb89df
Anjany Pandey <anjany.pandey06@gmail.com>
2024-11-25 19:48:13 +0530

    "Added async/await to /create route, added new /update route, and changed port from 3000 to 4000"

--- mongoDB_learn/app.js
+++ mongoDB_learn/app.js
@@ -6,13 +6,37 @@ app.get("/",(req, res)=>{
   res.send("hey");
   
 })
-app.get("/create",(req, res)=>{
-  userModel.create({
+app.get("/create", async(req, res)=>{
+  let createdUser =  await userModel.create({
     name : "john",
     email : "john@gmail.com",
     username : "john123"
   })
-  // yeh(userModel wla) hamra ek asynchronous operation hai mtlb ish line k baad ka code phle chl jayega
+  // yeh(userModel wla) hamra ek asynchronous operation hai mtlb ish line k baad ka code phle chl jayega toh isko synchronous bnane k liye hm async 
+  // await keyword ka use karte hai
+  res.send(createdUser);
   
 })
-app.listen("3000")
+
+app.get("/update" , async(req, res)=>{
+  let updatedUser = await userModel.findOneAndUpdate({username: "john123"},{name:"sohn"},{new : true})
+  res.send(updatedUser);
+})
+// yeh saare users read krta hai
+app.get('/read', async(req, res)=>{
+  let users = await userModel.find()
+  // find() hamra hr bar ek array deta hai
+  // if use findOne instead of find() then it will show blank screen instead of empty array.
+  // findOne hamesha hme phla user dega
+  res.send(users);
+})
+// if want to read specific user then pass username in find and it reads the specific user
+
+app.get('/delete', async(req, res)=>{
+  let deletedUser = await userModel.findOneAndDelete({username : "john123"});
+  res.send(deletedUser);
+  // so delete krne k baad ek baar hme user ka access milta joh ki issh variable me store hai
+})
+
+
+app.listen("4000")
